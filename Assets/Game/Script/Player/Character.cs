e54976ac406b2b182f0b10e52b0fddcfd2c0c149
MASHIRO\Mashiro <youho98zzz@gmail.com>
2023-05-21 22:23:04 +0900

    キャラクター移動を実装しました。

--- Assets/Game/Script/Player/Character.cs
+++ Assets/Game/Script/Player/Character.cs
@@ -9,8 +9,10 @@ namespace Player {
         private CharacterController _characterController;
         public float MoveSpeed = 5f;
         private Vector3 _movementVelocity;
+        private float _verticalVelocity;
         private PlayerInput _playerInput;
 
+        private float gravity = -20f;
         private void Awake() {
             _characterController = GetComponent<CharacterController>();
             _playerInput = GetComponent<PlayerInput>();
@@ -25,6 +27,13 @@ namespace Player {
 
         private void FixedUpdate() {
             CalculatePlayerMovement();
+
+            if (_characterController.isGrounded == false) {
+                _verticalVelocity = gravity;
+            }else {
+                _verticalVelocity = gravity * 0.3f;
+            }
+            _movementVelocity += _verticalVelocity * Vector3.up * Time.deltaTime;
             _characterController.Move(_movementVelocity);
         }
     }
