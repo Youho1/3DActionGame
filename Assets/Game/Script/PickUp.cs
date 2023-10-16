562060e0ac0317888020e3a2e7de58e2c48c983f
youho98zzZ <105184541+Youho1@users.noreply.github.com>
2023-10-16 10:50:05 +0900

    Update PickUp.cs
    説明文を追加

--- Assets/Game/Script/PickUp.cs
+++ Assets/Game/Script/PickUp.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    // 拾ったアイテムのタイプ
     public enum PickUpType {
         Heal,
         Coin,
     }
 
     public PickUpType Type;
+    //　回復量 or コインの数
     public int Value = 20;
+    // プレイヤーがアイテムを拾ったら、アイテムを削除
     private void OnTriggerEnter(Collider other) {
         if(other.tag=="Player"){
             other.gameObject.GetComponent<Player.PlayerController>().PickUpItem(this);
