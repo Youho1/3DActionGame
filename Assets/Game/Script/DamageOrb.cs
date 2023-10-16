015647d690f416ce2babc4fa7569265c0cee753e
youho98zzZ <105184541+Youho1@users.noreply.github.com>
2023-10-16 10:47:36 +0900

    Update DamageOrb.cs
    コードの説明文を追加

--- Assets/Game/Script/DamageOrb.cs
+++ Assets/Game/Script/DamageOrb.cs
@@ -23,12 +23,15 @@ public class DamageOrb : MonoBehaviour
     {
         // 侵入したキャラクターのタイプを判定　（Player.CharacterとはPlayerフォルダのCharacter.csファイルを指す）
         Player.Character cc = other.gameObject.GetComponent<Player.Character>();
-
+        // ダメージを適用
         if (cc != null && cc.IsPlayer)
         {
+        // 位置を渡す理由はダメージを受ける時の衝撃の方向を計算するため
             cc.ApplyDamage(Damage, transform.position);
         }
+        // VFXを生成
         Instantiate(HitVFX, transform.position, Quaternion.identity);
+        // 弾を削除
         Destroy(gameObject);
     }
 }
